20a7a438651ad9dc99d55dd03ddc05cb35888a74
Bolexzy <boluoyewumi79@gmail.com>
2023-08-06 02:31:03 +0100

    Fix: type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     ''' Filters/obfuscate a log message.
     '''
     pattern = r'(' + '|'.join(map(re.escape, fields)) +\
