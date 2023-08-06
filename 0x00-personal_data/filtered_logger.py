aad8c05f0f47b3efe1f5a727ad9f87133e47a8dd
Bolexzy <boluoyewumi79@gmail.com>
2023-08-06 02:29:52 +0100

    Add: task 0- Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' A module for filtering logs.
+'''
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str):
+    ''' Filters/obfuscate a log message.
+    '''
+    pattern = r'(' + '|'.join(map(re.escape, fields)) +\
+              r')=(.*?)(?=' + re.escape(separator) + '|$)'
+    return re.sub(pattern, rf'\1={redaction}', message)
