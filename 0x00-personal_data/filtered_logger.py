144b52250a32522df3bc6556d386e1f6b331627c
Bolexzy <boluoyewumi79@gmail.com>
2023-08-07 18:42:35 +0100

    Fix: typo

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,7 +19,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                 host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
                 user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
                 password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-                databse=os.getenv('PERSONAL_DATA_DB_NAME', ''),
+                database=os.getenv('PERSONAL_DATA_DB_NAME', ''),
                 port=3306,
                 )
 
