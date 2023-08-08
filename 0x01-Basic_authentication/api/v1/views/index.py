2a8bd7c1801379aba6b2c1c27499df39c67c0573
Bolexzy <boluoyewumi79@gmail.com>
2023-08-08 19:11:29 +0100

    Add: task2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,13 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    ''' Raise a 403 forbidden error.
+    '''
+    abort(403, description="Forbidden")
+
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     ''' Raise a 401 error
