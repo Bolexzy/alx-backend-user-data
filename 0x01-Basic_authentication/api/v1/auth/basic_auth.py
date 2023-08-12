e095b9bdddd7fc4c944820610d5f30c2c0c320f2
Bolexzy <boluoyewumi79@gmail.com>
2023-08-12 23:41:51 +0100

    Add: task7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 '''Basic authentication module for the API.
 '''
+import base64
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     ''' Represents Basic authentication.
     '''
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        ''' Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        '''
+        if isinstance(authorization_header, str):
+            if authorization_header.startswith("Basic "):
+                return authorization_header[6:]
+        else:
+            return None
--- 0x01-Basic_authentication/main.py
+++ 0x01-Basic_authentication/main.py
@@ -1,6 +1,14 @@
-#!/usr/bin/bash
+#!/usr/bin/env python3
+""" Main 2
+"""
+from api.v1.auth.basic_auth import BasicAuth
 
-curl "http://0.0.0.0:5000/api/v1/status"
-curl "http://0.0.0.0:5000/api/v1/status/"
-curl "http://0.0.0.0:5000/api/v1/users"
-curl "http://0.0.0.0:5000/api/v1/users" -H "Authorization: Test"
+a = BasicAuth()
+
+print(a.extract_base64_authorization_header(None))
+print(a.extract_base64_authorization_header(89))
+print(a.extract_base64_authorization_header("Holberton School"))
+print(a.extract_base64_authorization_header("Basic Holberton"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.extract_base64_authorization_header("Basic1234"))
