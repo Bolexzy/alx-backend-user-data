8aeb9feef016fa145e3a6f01704c00e6b4dfa7bf
Bolexzy <boluoyewumi79@gmail.com>
2023-08-14 00:08:57 +0100

    Add: task4. Read and filter data;

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,6 +17,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     '''
     connection = mysql.connector.connect(
                 host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+                port=3306,
                 user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
                 password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
                 database=os.getenv('PERSONAL_DATA_DB_NAME', ''),
@@ -76,3 +77,25 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = r'(' + '|'.join(map(re.escape, fields)) +\
               r')=(.*?)(?=' + re.escape(separator) + '|$)'
     return re.sub(pattern, rf'\1={redaction}', message)
+
+
+def main():
+    """Logs the information about user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
