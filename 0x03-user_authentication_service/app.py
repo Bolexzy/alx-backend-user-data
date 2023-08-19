d3035c4d0a54e51160423dff20cafeab660c08f3
Bolexzy <boluoyewumi79@gmail.com>
2023-08-19 17:56:44 +0100

    Add: task18. Update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,7 +76,7 @@ def profile():
     abort(403)
 
 
-@app.route('reset_password', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     ''' POST /reset_password
     Return:
